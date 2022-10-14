812c5927f5d3da4c1b3d3b8aa0ab61e54435285b
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2022-10-14 02:00:02 -0300

    Proyecto

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+            $('#menuInicio'.click(function() {
+                            $('#inicio'.animatescroll();
+                            });
+
+                        $('#menuServicios'.click(function () {
+                                    $('#servicios'.animatescroll();
+                                    });
+
+                                $('#menuPortfolio'.click(function () {
+                                            $('#portfolio'.animatescroll();
+                                            });
+
+                                        $('#menuContacto'.click(function () {
+                                                $('#Contacto'.animatescroll();
+                                                });
+
+                                        });
